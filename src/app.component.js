bf56c312314b3935c66414c1ae8831097ef97bf5
Ostap Dutkevych <od@inqud.com>
2023-09-04 23:22:11 +0300

    delete code

--- src/app.component.js
+++ src/app.component.js
@@ -17,16 +17,12 @@
     $ctrl.$onChanges = onChanges;
     $ctrl.$onDestroy = onDestroy;
     $ctrl.people = dataService.getUsers();
-    $ctrl.showModal = false;
     $ctrl.openModal = function (){
-      modalService.showModal();
-      $ctrl.showModal = modalService.show;
-      console.log('openModal')
-      console.log($ctrl.showModal)
+      console.log('OpenModal')
+      modalService.open();
     }
     $ctrl.createUser = function (){
       console.log('createUser')
-      console.log(123213)
     }
     $ctrl.title = 'Profile Test';
 
